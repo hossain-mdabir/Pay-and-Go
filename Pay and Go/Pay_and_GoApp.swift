492afcdee958521e2c6653f76a9421e36ae4a673
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-05-03 21:15:58 +0600

    Initial Commit

---
+++ Pay and Go/Pay_and_GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pay_and_GoApp.swift
+//  Pay and Go
+//
+//  Created by Md Abir Hossain on 03-05-2023.
+//
+
+import SwiftUI
+
+@main
+struct Pay_and_GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
